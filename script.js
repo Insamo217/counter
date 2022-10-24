bd3b85bcb62dc91757810c7c134291924d63e0be
Insamo <insamo217@yandex.ru>
2022-10-24 20:56:16 +0300

    first_commit

---
+++ script.js
@@ -0,0 +1,42 @@
+// Напишите функцию countDown(n), которая получает число {n} в качестве аргумента
+// и печатает в консоль обратный отсчет, начиная с этого числа.
+// Например, countDown(3) напечатает: 3 2 1.
+
+window.onload = function () {
+  const push = document.querySelector(".btn");
+  const counter = document.querySelector(".count");
+  const input = document.querySelector(".input");
+  const flex = document.querySelector(".flex");
+  const text = document.querySelector(".text");
+
+  input.addEventListener("keyup", numKey);
+  push.addEventListener("click", countDown);
+
+  let countNum;
+
+  function numKey(e) {
+    countNum = e.key;
+  }
+
+  function countDown(n) {
+    n = countNum;
+    flex.style.display = "none";
+    text.style.display = "none";
+    const textCount = document.createElement("p");
+    counter.appendChild(textCount);
+    const boom = () => {
+      textCount.textContent = "БУМ";
+    };
+    const count = setInterval(() => {
+      while (n >= 0) {
+        textCount.textContent = n;
+        n--;
+        if (n === 0) {
+          clearInterval(count);
+          setTimeout(boom, 2000);
+        }
+        break;
+      }
+    }, 1000);
+  }
+};
